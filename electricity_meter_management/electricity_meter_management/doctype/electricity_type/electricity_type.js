5520b1d712000b1021fc429c0c812bc44bb9a6fb
aliahmedprositeyemen <a.ahmed@prositeyemen.com>
2025-12-08 21:23:52 +0300

    add child table

---
+++ electricity_meter_management/electricity_meter_management/doctype/electricity_type/electricity_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, alipro and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Electricity Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
