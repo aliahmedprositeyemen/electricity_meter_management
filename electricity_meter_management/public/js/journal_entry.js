934258e905feac808f2bbea919b5bbfb1802f873
aliahmedprositeyemen <a.ahmed@prositeyemen.com>
2026-07-20 18:54:52 +0300

    correct 1 to 0

--- electricity_meter_management/public/js/journal_entry.js
+++ electricity_meter_management/public/js/journal_entry.js
@@ -2,9 +2,9 @@ frappe.ui.form.on('Journal Entry', {
     refresh: function (frm) {
         // Hide "Duplicate" menu for users without the "Accounts Manager" role
         if (!frappe.user.has_role("Accounts Manager")) {
-            frm.meta.allow_copy = 0;
-        } else {
             frm.meta.allow_copy = 1;
+        } else {
+            frm.meta.allow_copy = 0;
         }
 
         if (frm.toolbar) {
